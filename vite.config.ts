452fce7f1ae61ef78c7b9fb305020dc5145d76e9
Guillaume Larocque <glaroc@gmail.com>
2025-03-14 13:40:50 -0400

    First functioning version

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
-  base: "/viewer",
+  base: "/",
   sourcemap: false,
   build: {
     chunkSizeWarningLimit: 100,
